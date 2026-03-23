85ffcac5608edddb8240917bcdb8f5be592695b3
samui <samuiker2427@gmail.com>
2026-03-23 16:28:27 +0100

    Solucionar problemas CORS

--- src/main/java/com/example/backend/web/AuthController.java
+++ src/main/java/com/example/backend/web/AuthController.java
@@ -9,10 +9,12 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+// Problemas con CORS solucionados para aceptar todos los origenes
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    // 
+
     private final AuthService authService;
 
     public AuthController(AuthService authService) {
@@ -20,34 +22,8 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest req) {
-
-        String token = authService.login(req.username(),req.password());
-
-        ResponseCookie adminCookie = ResponseCookie.from("ADMIN_AUTH", token)
-                .httpOnly(true)
-                .secure(false) // true en HTTPS
-                .path("/") // o "/admin.html" si quieres limitar más (ver nota abajo)
-                .sameSite("Lax")
-                .maxAge(Duration.ofSeconds(1800))
-                .build();
-        return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, adminCookie.toString())
-                .body(new LoginResponse(token));
-    }
-
-    @PostMapping("/logout")
-    public ResponseEntity<Void> logout() {
-        ResponseCookie cookie = ResponseCookie.from("ADMIN_AUTH", "")
-                .httpOnly(true)
-                .secure(false)
-                .sameSite("Lax")
-                .path("/")
-                .maxAge(0)
-                .build();
-
-        return ResponseEntity.noContent()
-                .header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .build();
+    public LoginResponse login(@RequestBody LoginRequest request) {
+        String token = authService.login(request.username(), request.password());
+        return new LoginResponse(token);
     }
 }
